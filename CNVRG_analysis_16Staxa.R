aa0b64038717b5fd3bc7fd5be57920273d18468b
J. G. Harrison <jgharrison1982@gmail.com>
2021-03-10 10:27:10 -0700

    first commit

---
+++ CNVRG_analysis_16Staxa.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+library(CNVRG)
+dat <- read.csv("./16S_otuTable_arranged_by_taxon.csv",
+                  fill = T, header = T, stringsAsFactors = F)
+
+metadat <- read.csv("./metadata_wrangled.csv", stringsAsFactors = F)
+table(dat$X == metadat$Identifier)
+table(metadat$Taxon)
+
+#add a one to our data
+#also remove any zeros
+#no zeros
+table(colSums(dat[,2:(length(dat)-2)]) == 0)
+dat[,2:length(dat)] <- dat[,2:length(dat)] + 1
+
+#Metal AF!
+set.seed(666)
+
+modelOut <- cnvrg_VI(
+  countData = dat,
+  starts = indexer(metadat$Taxon)$starts,
+  ends = indexer(metadat$Taxon)$ends,
+  output_samples = 500,
+  params_to_save = c("pi", "p")
+)
+save.image(file = "./CNVRG_16Staxon.Rdata")
+diffs <- diff_abund(model_output = modelOut, countData = dat)
+
+save.image(file = "./CNVRG_16Sdiffstaxon.Rdata")
+
